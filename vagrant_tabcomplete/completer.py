3c96f4750d8d6c85bd4bd482512df36a60da393c
Jon Goodger <jonno.is@gmail.com>
2019-09-18 01:12:24 +0100

    Complete partial top level commands

--- vagrant_tabcomplete/completer.py
+++ vagrant_tabcomplete/completer.py
@@ -2,7 +2,7 @@ import re
 
 
 class Completer:
-    command_list = {
+    top_level_commands = {
         'box',
         'cloud',
         'connect',
@@ -42,6 +42,7 @@ class Completer:
         self.endidx = endidx
         self.ctx = ctx
         self.tokens = re.split(r'\s+', line.strip())
+        self.wants_next_val = line.endswith(' ')
 
     def should_complete(self):
         return self.tokens[0] == 'vagrant'
@@ -50,9 +51,32 @@ class Completer:
         if not self.should_complete():
             return
 
-        return self.command_list
+        # If we only have `vagrant`, list all top level commands
+        if len(self.tokens) == 1:
+            return self.top_level_commands
+
+        # Otherwise find the completer for the top level command, "vagrant rsync-auto" dispatches to complete_rsync_auto() for example
+        if self.wants_next_val:
+            return self.complete_subcommand()
+        else:
+            return self.complete_top_level_command()
+
+    def complete_subcommand(self):
+        command = self.tokens[1]
+        method_name = "complete_%s" % command.lower().replace('-', '_')
+
+        try:
+            method = getattr(self, method_name)
+        except AttributeError:
+            # We can't complete this
+            return
+
+        return method()
+
+    def complete_top_level_command(self):
+        return {x for x in self.top_level_commands if x.startswith(self.prefix)}
 
     @classmethod
-    def complete(cls, prefix, line, begidx, endidx, ctx=None):
+    def complete(cls, prefix, line, begidx, endidx, ctx):
         comp = cls(prefix, line, begidx, endidx, ctx)
         return comp._complete()
